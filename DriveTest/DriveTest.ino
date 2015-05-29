8790b8c81c026d2789b33ec740ad8605ab4dff0f
Mike Raben <raben.mike@gmail.com>
2015-05-29 16:20:48 -0600

    Navigation working
    Smoothed turning to heading
    Changed to analogWrite to control speed

--- DriveTest/DriveTest.ino
+++ DriveTest/DriveTest.ino
@@ -18,7 +18,7 @@ Servo myServo;
 int servoL = 135;
 int servoC = 90;
 int servoR = 45;
-int servoDelay = 300;
+int servoDelay = 150;
 int servoPin = 10;
 
 int fw = 3;
@@ -26,26 +26,26 @@ int rv = 5;
 int lf = 6;
 int rt = 9;
 
-int turnDelay = 250; //update this to correct over/understeer
+int driveSpeed = 110;
+int turnSpeed = 255;
+float wiggle = 2.5;
 
-float stopDist = 100.0;
-int targetHeading;
-long distTraveled = 0;
-int legHeading [4] = {0, 90, 180, 270};
-int legDist [5] = {10, 20, 30, 40};
+int turnDelay = 350;
 
-bool moving;
+float stopDist = 85.0;
+
+float targetHeading;
+long distTraveled;
+int legHeading [4] = {0.0, 90.0, 180.0, 270.0};
+int legDist [4] = {10, 20, 30, 40};
+
+bool moving, frontClear, rightClear, leftClear;
 
 RunningMedian samples = RunningMedian(30);
 
 void setup() {
-  Serial.begin(9600); //Opens serial connection at 9600bps.
+  Serial.begin(9600);
   distTraveled = 0;
-
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(9, OUTPUT);
 
   //compass setup
   Wire.begin();
@@ -66,8 +66,8 @@ void setup() {
   myServo.write(servoC);
 
   //Setup wheel encoder
-  //  pinMode(encoderPin, INPUT);
-  //  attachInterrupt(0, encoderTick, HIGH);
+  pinMode(encoderPin, INPUT);
+  attachInterrupt(0, encoderTick, HIGH);
   moving = false;
 }
 
@@ -113,38 +113,71 @@ int checkCompass() {
 }
 
 void navigate() {
-  Serial.println("Navigate: ");
-  Serial.println(checkCompass());
+  if (distTraveled < legDist[0]) {
+    targetHeading = legHeading[0];
+  }
+  if (distTraveled > legDist[0] && distTraveled < legDist[1]) {
+    targetHeading = legHeading[1];
+  }
+  if (distTraveled > legDist[1] && distTraveled < legDist[2]) {
+    targetHeading = legHeading[2];
+  }
+  if (distTraveled > legDist[2]){
+    targetHeading = legHeading[3];
+  }
+
+  float currentHeading = checkCompass();
+
+  if (currentHeading < targetHeading - wiggle) {
+    while (checkCompass() < targetHeading - wiggle) {
+      analogWrite(rt, turnSpeed);
+    }
+    analogWrite(rt, 0);
+  }
+  if (currentHeading > targetHeading + wiggle) {
+    while (checkCompass() > targetHeading + wiggle) {
+      analogWrite(lf, turnSpeed);
+    }
+    analogWrite(lf, 0);
+  }
 }
 
-void drive() {  
+void sweep() {
   myServo.write(servoC);
   delay(servoDelay);
-  bool frontClear = measure() > stopDist;
-  
+  frontClear = measure() > stopDist;
+
+  myServo.write(servoR);
+  delay(servoDelay);
+  rightClear = measure() > stopDist;
+
+  myServo.write(servoL);
+  delay(servoDelay);
+  leftClear = measure() > stopDist;
+
+  myServo.write(servoC);
+  delay(servoDelay);
+}
+
+void drive() {
+  myServo.write(servoC);
+  delay(servoDelay);
+  frontClear = measure() > stopDist;
+
   if (frontClear) {
-    Serial.println("Front Clear, go forward");
     forward();
     navigate();
   } else {
-    Serial.println("Front blocked looking right");
     stopAll();
-    myServo.write(servoR);
-    delay(servoDelay);
-    if (measure() > stopDist) {
-      Serial.println("Right clear turning right");
+    sweep();
+    if (rightClear) {
       forward();
       rightTurn();
     } else {
-      Serial.println("F and R blocked looking L");
-      myServo.write(servoL);
-      delay(servoDelay);
-      if (measure() > stopDist) {
-        Serial.println("L clear turning Left");
+      if (leftClear) {
         forward();
         leftTurn();
       } else {
-        Serial.println("All blocked reversing");
         reverse();
         stopAll();
       }
@@ -157,44 +190,39 @@ void encoderTick() {
 }
 void forward()
 {
-  Serial.println("FORWARD");
-  if (!moving){
-    digitalWrite(rv, LOW);
-    digitalWrite(fw, HIGH);
+  if (!moving) {
+    analogWrite(rv, 0);
+    analogWrite(fw, driveSpeed);
     moving = true;
   }
 }
 
 void reverse()
 {
-  Serial.println("REVERSE");
-  digitalWrite(fw, LOW);
-  digitalWrite(rv, HIGH);
+  analogWrite(fw, 0);
+  analogWrite(rv, 200);
   delay(500);
-  digitalWrite(rv, LOW);
+  analogWrite(rv, 0);
 
 }
 void stopAll()
 {
-  Serial.println("STOP ALL");
-  digitalWrite(fw, LOW);
-  digitalWrite(rv, LOW);
-  digitalWrite(lf, LOW);
-  digitalWrite(rt, LOW);
+  analogWrite(fw, 0);
+  analogWrite(rv, 0);
+  analogWrite(lf, 0);
+  analogWrite(rt, 0);
   moving = false;
 }
 
 void leftTurn()
 {
-  Serial.println("LEFT");
-  digitalWrite(lf, HIGH);
+  analogWrite(lf, turnSpeed);
   delay(turnDelay);
-  digitalWrite(lf, LOW);
+  analogWrite(lf, 0);
 }
 void rightTurn()
 {
-  Serial.println("RIGHT");
-  digitalWrite(rt, HIGH);
+  analogWrite(rt, turnSpeed);
   delay(turnDelay);
-  digitalWrite(rt, LOW);
+  analogWrite(rt, 0);
 }
